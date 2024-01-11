2f2463f5176625baaebc2c4b972ad1c3fbe10501
opilane <opilane@HP-CZC2349HTN>
2024-01-11 12:29:31 +0200

    djfjgb

--- RomanSandakov1.TARpv23.py
+++ RomanSandakov1.TARpv23.py
@@ -24,3 +24,44 @@ print("Kokku:",kokku)
 mitu=int(input("Mitu kommi tahad võtta?"))
 kokku=mitu
 print("Laua peal on",kokku,"kommid")
+#4
+#dlina=int(input("Mis on dlina?"))
+#diametr=dlina*3.14
+#print(diametr)
+#5
+#H=int(input("Mis on H?"))
+#M=int(input("Mis on M?"))
+#diagonal=**H^2*M^2
+#print(diagonal)
+#6
+#aeg = float(input("Mitu tundi kulus sõiduks? "))
+#teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
+#kiirus = teepikkus / aeg
+#print("Sinu kiirus oli " + float(kiirus) + " km/h")
+#number1=int(input("What number?" ))
+#number2=int(input("What number?" ))
+#number3=int(input("What number?" ))
+#number4=int(input("What number?" ))
+#number5=int(input("What number?" ))
+#middlenumber = (number1 + number2 + number3 + number4 + number5)/5
+#print("keskmise suvalisest:" + str(middlenumber))
+#8
+
+#frog ="""
+#    @..@
+#   (----)
+#  ( \__/  )
+#^^ "" ^^
+#"""
+#print(frog)
+#9
+#A=int(input("onece side:"))
+#B=int(input("twice side:"))
+#C=int(input("third side:"))
+#P=A+B+C
+#print("ümbermõõt:" + str(P))
+#10
+#pizza=int(12.90)
+#chay=pizza/100*10
+#summa=pizza+chay
+#print("You pay:" + str(summa/2) + " friend pay:" + str(summa/2))
